c401eafe613059dc61357cd186921f120b29eac1
Daniel Gladson Arulraj P <danielgladson55@gmail.com>
2026-07-29 11:05:35 +0400

    Add searchControls function for filtering controls

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,1 +1,10 @@
-
+export const searchControls = (
+  controls,
+  searchTerm
+) => {
+  return controls.filter(control =>
+    control.controlName
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+};
