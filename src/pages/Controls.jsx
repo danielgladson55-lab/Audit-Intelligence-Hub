4ed5981fc2a2f90dae6e8184cfcb1c252203c1f9
Daniel Gladson Arulraj P <danielgladson55@gmail.com>
2026-07-29 11:10:14 +0400

    Implement Controls component to display control cards

--- src/pages/Controls.jsx
+++ src/pages/Controls.jsx
@@ -1,1 +1,21 @@
+import controls from "../data/controls";
+import ControlCard from "../components/ControlCard";
 
+export default function Controls() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-5">
+        Control Repository
+      </h2>
+
+      <div className="grid gap-4">
+        {controls.map(control => (
+          <ControlCard
+            key={control.controlId}
+            control={control}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
