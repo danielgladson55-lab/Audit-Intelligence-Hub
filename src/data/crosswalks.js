8316a65046bdca6e42fb0fc4bc9e45fb2a476e49
Daniel Gladson Arulraj P <danielgladson55@gmail.com>
2026-07-29 11:05:03 +0400

    Add crosswalks data for security controls

--- src/data/crosswalks.js
+++ src/data/crosswalks.js
@@ -1,1 +1,30 @@
+const crosswalks = [
+  {
+    keyword: "Multi-Factor Authentication",
+
+    mappings: {
+      iso27001: ["5.17"],
+      nist80053: ["IA-2"],
+      cobit: ["DSS05"],
+      cis: ["6"],
+      pci: ["8"],
+      soc2: ["CC6"]
+    }
+  },
+
+  {
+    keyword: "Privileged Access",
+
+    mappings: {
+      iso27001: ["5.15"],
+      nist80053: ["AC-2"],
+      cobit: ["DSS05"],
+      cis: ["5"],
+      pci: ["7"],
+      soc2: ["CC6"]
+    }
+  }
+];
+
+export default crosswalks;
 
